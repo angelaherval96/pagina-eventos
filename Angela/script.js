7e0f0e6c5e06b9b41fafdda5908e115ce5f3cc30
Ángela Hernández <angelaherval96@digitechfp.com>
2024-11-08 10:36:13 +0100

    estructura

--- Angela/script.js
+++ Angela/script.js
@@ -1,6 +1,27 @@
 const artistas = document.querySelectorAll(".slider");
 let index = 0;
 
-mostrarArtistas(){
-    
+function mostrarArtistas(){
+    artistas[index].classList.remove('active');
+
+    index = (index + 1) % artistas.length;
+   
+    artistas[index].classList.add("active");
 }
+
+function ejecutarSlider(){
+    setInterval(mostrarArtistas, 3000);
+}
+
+ejecutarSlider();
+
+const artista1 = document.querySelectorAll(".artista1");
+artista1.addEventListener('mouseover', () => {
+    artista1.innerHTML('selena') = ('Selena Gomez');
+
+}
+)
+    
+
+
+    
