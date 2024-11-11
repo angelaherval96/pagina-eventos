272d08907458dc4638eb55c9fbb3bf578331ce6e
Nick <148863774+nicklasperez@users.noreply.github.com>
2024-11-11 09:57:17 +0100

    Nick Terminado sin responsive

--- Nicklas/script.js
+++ Nicklas/script.js
@@ -1,15 +1,47 @@
-const slides = document.querySelectorAll(".videos");
-console.log(slides[3]);
-var index = 0;
+const slidesFut = document.querySelectorAll(".videosFut");
+console.log(slidesFut[3]);
+var indexFut = 0;
 
-function siguienteVideo() {
-    slides[index].classList.remove("active");
-    index = (index + 1) % slides.length;
-    slides[index].classList.add("active");
+function siguienteVideoFut() {
+    slidesFut[indexFut].classList.remove("active");
+    indexFut = (indexFut + 1) % slidesFut.length;
+    slidesFut[indexFut].classList.add("active");
 }
 
-function previoVideo() {
-    slides[index].classList.remove("active");
-    index = (index + 2) % slides.length;
-    slides[index].classList.add("active");
+function previoVideoFut() {
+    slidesFut[indexFut].classList.remove("active");
+    indexFut = (indexFut + 2) % slidesFut.length;
+    slidesFut[indexFut].classList.add("active");
+}
+
+const slidesBask = document.querySelectorAll(".videosBask");
+console.log(slidesBask[3]);
+var indexBask = 0;
+
+function siguienteVideoBask() {
+    slidesBask[indexBask].classList.remove("active");
+    indexBask = (indexBask + 1) % slidesBask.length;
+    slidesBask[indexBask].classList.add("active");
+}
+
+function previoVideoBask() {
+    slidesBask[indexBask].classList.remove("active");
+    indexBask = (indexBask + 2) % slidesBask.length;
+    slidesBask[indexBask].classList.add("active");
+}
+
+const slidesF1 = document.querySelectorAll(".videosF1");
+console.log(slidesF1[3]);
+var indexF1 = 0;
+
+function siguienteVideoF1() {
+    slidesF1[indexF1].classList.remove("active");
+    indexF1 = (indexF1 + 1) % slidesF1.length;
+    slidesF1[indexF1].classList.add("active");
+}
+
+function previoVideoF1() {
+    slidesF1[indexF1].classList.remove("active");
+    indexF1 = (indexF1 + 2) % slidesF1.length;
+    slidesF1[indexF1].classList.add("active");
 }
